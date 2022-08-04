e111cbe9f97b49d6053e520bc8572eeeab1e6259
Ivan Minenok <ivan.minenok@gmail.com>
2022-08-04 05:15:29 -0500

    minenok1 - function /about/ has been added to app.py

--- py_env/app.py
+++ py_env/app.py
@@ -8,3 +8,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
+
+@app.route('/about/')
+def about():
+    return '<h3>About page that will explain u some things</h3>'
