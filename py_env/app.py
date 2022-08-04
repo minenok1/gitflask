9e09c756c69d70f4e00060cc2775651cc9d2b6bf
Ivan Minenok <ivan.minenok@gmail.com>
2022-08-04 06:55:16 -0500

    minenok1 - function /about,capitalize,add,greet_users has been added to app.py

--- py_env/app.py
+++ py_env/app.py
@@ -1,5 +1,4 @@
-
-
+from markupsafe import escape
 from flask import Flask
 
 app = Flask(__name__)
@@ -14,3 +13,23 @@ def hello():
 @app.route('/about/')
 def about():
     return '<h3>About page that will explain u some things</h3>'
+
+
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+
+
+@app.route('/add/<int:n1>/<int:n2>/')
+def add(n1, n2):
+    return '<h1>{}</h1>'.format(n1 + n2)
+
+
+@app.route('/users/<int:user_id>/')
+def greet_user(user_id):
+    users = ['Ivan', 'Sahsa', 'Lexus']
+    try:
+        return '<h2>Hi, {}</h2>'.format(users[user_id])
+    except IndexError:
+        abort(404)
+
